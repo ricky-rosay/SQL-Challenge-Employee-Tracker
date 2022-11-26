4a8d01e2dca2c524a2f7fb83fd0189d5e702a7d4
ricky-rosay <111669764+ricky-rosay@users.noreply.github.com>
2022-11-25 19:55:59 -0700

    adding msql connection

--- app.js
+++ app.js
@@ -7,3 +7,30 @@ let departments;
 let managers;
 let employees;
 
+var connection = mysql.createConnection({
+    host: "localhost",
+  
+    // Your port; if not 3306
+    port: 3306,
+  
+    // Your username to sql
+    user: "root",
+  
+    // Your password to sql 
+    password: "admin",
+    // name of data base that is being pulled from sql
+    database: "employee_db"
+  });
+
+  figlet('FSC Employee Tracker', (err, result) => {
+    console.log(err || result);
+  });
+
+  connection.connect(function(err) {
+    if (err) throw err;
+    start();
+    getDepartments();
+    getRoles();
+    getManagers();
+    getEmployees();
+  });
